45e85591f777d30fe5b2cb99aeb6947b2f7f442b
MertYksl03 <mert.yksl.03@gmail.com>
2026-04-28 18:33:25 +0300

    add missing function declaration

--- src/buttons.h
+++ src/buttons.h
@@ -26,6 +26,7 @@ typedef struct {
 Button *create_button(SDL_Renderer* renderer, ButtonType type, const char* svg_path, int x, int y, int size);
 SDL_Texture* LoadSVG(SDL_Renderer* renderer, const char* file, int width, int height);
 void render_button(SDL_Renderer* renderer, Button* button, SDL_Color color);
+bool is_button_hovered(Button* button, int mouse_x, int mouse_y);
 void handle_button_hover(Button* buttons[], int button_count, int mouse_x, int mouse_y);
 void destroy_button(Button* button);
 void bind_button_to_function(Button* button, void (*on_click)());
